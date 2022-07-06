535b06d0d592473fea89be1b1b56e20f1f872422
Aaron <aaronrw4@yahoo.com>
2022-07-06 03:44:33 -0500

    Created cache

--- jumpGame.js
+++ jumpGame.js
@@ -1,6 +1,7 @@
 var canJump = function(nums) {
     let lastIndex = nums.length - 1
     let result = false
+    let indexes = []
 
     if (nums.length === 1) {
         return true
@@ -8,14 +9,18 @@ var canJump = function(nums) {
 
     function jumpGame(index) {
         for (var i = nums[index]; i > 0; i--) {
-            console.log(index, i)
-            if (index + i === lastIndex) {
+            if (index + i >= lastIndex) {
                 result = true
-                break
-            } else if (i === 0 || index + i > lastIndex) {
-                continue
-            } else if (index + i < lastIndex) {
-                jumpGame(index + i)
+                break;
+            } else if (i === 0 ) {
+                continue;
+            } else {
+                if (indexes.includes(index + i) === true) {
+                    continue;
+                } else {
+                    indexes.push(index + i)
+                    jumpGame(index + i)
+                }
             }
         }
     }
@@ -24,4 +29,4 @@ var canJump = function(nums) {
     return result
 };
 
-console.log(canJump([4,0,4,2,2,0,1,3,3,0,3]))
+console.log(canJump([8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5]))
